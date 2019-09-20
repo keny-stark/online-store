3a5e3ff3562387bba6262abf8951bb807a3fb7da
keny-stark <abdurakhmanov_i@iuca.kg>
2019-09-20 17:37:43 +0600

    обновил фикстуры

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -10,8 +10,8 @@ product_status_choices = [('monitors', 'monitors'), ('processors', 'processors')
 class Products(models.Model):
     name = models.TextField(max_length=100, null=False, blank=False, verbose_name='Name')
     description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Description')
-    status = models.TextField(max_length=40, null=False, blank=False,
-                              choices=product_status_choices, default='other', verbose_name='Status')
+    status = models.TextField(max_length=40, null=False,
+                              blank=False, choices=product_status_choices, default='other', verbose_name='Status')
     balance = models.IntegerField(verbose_name='Balance')
     price = models.DecimalField(max_digits=7, verbose_name='Price', decimal_places=2)
 
