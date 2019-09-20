6f734fa037648fe85df9a474302992ec89c3b227
keny-stark <abdurakhmanov_i@iuca.kg>
2019-09-20 15:48:06 +0600

    применил фикстуры и настроил модели, в админки добавил первые данные и на них применил фикстуры

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+product_status_choices = [('monitors', 'monitors'), ('processors', 'processors'),
+                          ('other', 'other'), ('video card', 'video card'),
+                          ('components', 'components'), ('laptops', 'laptops'),
+                          ('motherboards', 'motherboards'), ('RAM', 'RAM'),
+                          ('cooling systems', 'cooling systems'), ('power supplies', 'power supplies'),
+                          ('cases', 'cases')]
 
-# Create your models here.
+
+class Products(models.Model):
+    name = models.TextField(max_length=100, null=False, blank=False, verbose_name='Name')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Description')
+    status = models.TextField(max_length=40, null=False, blank=False,
+                              choices=product_status_choices, default='other', verbose_name='Status')
+    balance = models.IntegerField(verbose_name='Balance')
+    price = models.DecimalField(max_digits=7, verbose_name='Price', decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
